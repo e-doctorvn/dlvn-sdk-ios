40fe3a6df34d320f4e90e902d3df5872f026032e
buidinhmanh <manhbd@edoctor.vn>
2023-12-28 10:38:26 +0700

    update code

--- Sources/EdoctorDlvnSdk/Utils/BackgroundImage.swift
+++ Sources/EdoctorDlvnSdk/Utils/BackgroundImage.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 
-//               Image("backgroundCall", bundle: isCocoapod ? Bundle(for: WebViewController.self) : Bundle.module)
+//Image("backgroundCall", bundle: isCocoapod ? Bundle(for: WebViewController.self) : Bundle.module)
 @available(iOS 14.3, *)
 struct BackgroundImage: View, Encodable {
     let UrlString: String?
@@ -23,7 +23,7 @@ struct BackgroundImage: View, Encodable {
                     .blur(radius: blur)
                     .opacity(0.7)
             } placeholder: {
-                Image("backgroundCall", bundle: Bundle.module)
+                Image("backgroundCall", bundle: Bundle(for: WebViewController.self))
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
@@ -35,7 +35,7 @@ struct BackgroundImage: View, Encodable {
                     .edgesIgnoringSafeArea(.all)
                     .blur(radius: blur)
             } else {
-                Image("backgroundCall", bundle: Bundle.module)
+                Image("backgroundCall", bundle: Bundle(for: WebViewController.self))
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
