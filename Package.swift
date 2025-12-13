2381af7ab940e04415a94a0269af5fcf7b4e80f4
Đạt <dattm@edoctor.vn>
2025-12-13 13:57:52 +0700

    chore: Remove commented-out Firebase, Apollo, and local API package dependencies.

--- Package.swift
+++ Package.swift
@@ -19,9 +19,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/sendbird/sendbird-calls-ios.git", from: "1.11.1"),
         .package(url: "https://github.com/sendbird/sendbird-chat-sdk-ios.git", from: "4.34.1"),
-//        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.24.0"),
-//        .package(url: "https://github.com/apollographql/apollo-ios.git", from: "0.36.0"),
-//        .package(path: "./API")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -31,19 +28,12 @@ let package = Package(
             dependencies: [
                 .product(name: "SendBirdCalls", package: "sendbird-calls-ios"),
                 .product(name: "SendbirdChatSDK", package: "sendbird-chat-sdk-ios"),
-//                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
-//                .product(name: "Apollo", package: "apollo-ios"),
-//////                .product(name: "ApolloWebSocket", package: "apollo-ios-websocket"),
-//                .product(name: "API", package: "API"),
             ]),
         .testTarget(
             name: "EdoctorDlvnSdkTests",
             dependencies: [
                 .product(name: "SendBirdCalls", package: "sendbird-calls-ios"),
                 .product(name: "SendbirdChatSDK", package: "sendbird-chat-sdk-ios"),
-//                .product(name: "Apollo", package: "apollo-ios"),
-//
-//                .product(name: "API", package: "API"),
             ]),
     ]
 )
